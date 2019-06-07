33af95ae6839c1ea345152a717f591146c116b8f
Anne-E <aepinning@hotmail.com>
2019-06-07 20:29:53 +0200

    split ui and logic, begin unitest

--- CountOnMe/ViewController.swift
+++ CountOnMe/ViewController.swift
@@ -8,137 +8,32 @@
 
 import UIKit
 
-let operators = ["+", "x", "-", "/", "="]
-
 class ViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet var numberButtons: [UIButton]!
     
-    var elements: [String] {
-        return textView.text.split(separator: " ").map { "\($0)" }
-    }
-    
-    var expressionHaveEnoughElement: Bool {
-        return elements.count >= 3
-    }
-    
-    var expressionHaveResult: Bool {
-        return textView.text.firstIndex(of: "=") != nil
-    }
+    var simpleCalc: SimpleCalc = SimpleCalc()
     
     // View Life cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
-    func canAddComa() -> Bool {
-            if let lastElement = elements.last {
-                if lastElement.contains(".") {
-                    return false
-                }
-                for character in operators {
-                    if lastElement.contains(character) {
-                        return false
-                    }
-                }
-            } else {
-                return false
-            }
-        return true
+    // Updqte UI and Logic expression
+    func updateExpression(_ newExpression: String) {
+        textView.text = newExpression
+        simpleCalc.expression = newExpression
     }
     
-    func canAddOperator() -> Bool {
-        if let lastElement = elements.last {
-            for character in operators {
-                if lastElement.contains(character) {
-                    return false
-                }
-            }
-        } else {
-            return false
-        }
-        return true
-    }
-    
-    func calcResult() {
-        
-        var iterator = 0
-        
-        while iterator < elements.count - 1 {
-            let element = elements[iterator]
-            
-            if element == "x" || element == "/" {
-                let prevElement = elements[iterator - 1]
-                let nextElement = elements[iterator + 1]
-                
-                var res : Float = 0
-                
-                if element == "x" {
-                    res = Float(prevElement)! * Float(nextElement)!
-                } else {
-                    res = Float(prevElement)! / Float(nextElement)!
-                }
-                
-                let toReplace = prevElement + " " + element + " " + nextElement
-                
-                self.textView.text = self.textView.text.replacingOccurrences(of: toReplace, with: String(res))
-                    calcResult()
-                    return
-                
-            }
-            iterator += 1
-        }
-        
-        iterator = 0
-        
-        while iterator < elements.count - 1 {
-            let element = elements[iterator]
-            
-            if element == "+" || element == "-" {
-                let prevElement = elements[iterator - 1]
-                let nextElement = elements[iterator + 1]
-                
-                var res : Float = 0
-                
-                if element == "+" {
-                    res = Float(prevElement)! + Float(nextElement)!
-                } else {
-                    res = Float(prevElement)! - Float(nextElement)!
-                }
-                
-                let toReplace = prevElement + " " + element + " " + nextElement
-                
-                self.textView.text = self.textView.text.replacingOccurrences(of: toReplace, with: String(res))
-                calcResult()
-                return
-                
-            }
-            iterator += 1
-        }
-    }
-    
-    // View actions
+    // Func that manages all the buttons of the calculator
     @IBAction func tappedButton(_ sender: UIButton) {
         guard let numberText = sender.title(for: .normal) else {
             return
         }
         
-        if expressionHaveResult {
-            textView.text = ""
-        }
-        
-        if numberText == "." && !canAddComa() {
-            return
-        }
-        
-        if operators.contains(numberText) && !canAddOperator() {
-            return
-        }
-        
         if numberText == "=" {
-            if expressionHaveEnoughElement {
-                calcResult()
+            if simpleCalc.expressionHaveEnoughElement {
+                updateExpression(simpleCalc.calcResult())
             }
             else {
                 return
@@ -146,15 +41,10 @@ class ViewController: UIViewController {
         }
         
         if numberText == "AC" {
-            textView.text = ""
+            updateExpression("")
         } else {
-            if (operators.contains(numberText)) {
-                textView.text.append(" ")
-            }
-            textView.text.append(numberText)
-            if (operators.contains(numberText)) {
-                textView.text.append(" ")
-            }
+            simpleCalc.addToExpression(numberText)
+            updateExpression(simpleCalc.expression)
         }
     }
 }
--- CountOnMeTests/SimpleCalcTests.swift
+++ CountOnMeTests/SimpleCalcTests.swift
@@ -6,29 +6,46 @@
 //  Copyright © 2019 Vincent Saluzzo. All rights reserved.
 //
 
-import XCTest
-@testable import SimpleCalc
+// Test 1 - Deux operateurs ne peuvent pas se suivre
+// Test 2 - Un cacule doit compoter deux nombres et un operateur pour être valide
+// Test 3 - Tester une addition
+// Test 4 - Tester une soustraction
+// Test 5 - Tester une division
+// Test 6 - Tester une multiplication
+// Test 7 - Tester une devision par 0
+// Test 8 - Tester une addition suivie d'une multiplication (priorité)
+// Test 9 - Tester un calcul menant a un resultat trop grand
 
-class SimpleCalcTests: XCTestCase {
+// Test 10 - Tester qu'un nombre ne puisse pas comporter deux fois de virgule
+// Test 11 - Tester un calcul menant un resultat negatif
+
+
+import XCTest
+@testable import CountOnMe
+
+class CountOnMeTests: XCTestCase {
+    
+    var simpleCalc: SimpleCalc = SimpleCalc()
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        simpleCalc = SimpleCalc()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testDoubleOperator() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        
+        simpleCalc.addToExpression("1")
+        simpleCalc.addToExpression("+")
+        simpleCalc.addToExpression("+")
+        
+        XCTAssert(simpleCalc.expression == "1 + ")
     }
 
 }
